38372c2e3f6f4f68f77047e476c574cc759def68
Piotr Zoladz <zoladzp@ads.autodesk.com>
2016-05-31 17:00:30 +0200

    Different format of emails file, and switch to dictionaries.

--- mailer.py
+++ mailer.py
@@ -1,17 +1,24 @@
-emails = {}
+def get_emails():
+    emails = {}
 
-try:
-    email_file = open('emails2.txt', 'r')
+    try:
+        email_file = open('emails2.txt', 'r')
 
-    for line in email_file:
-        (email, name) = line.split(',')
-        emails[email] = name
-except FileNotFoundError as err:
-    print(err)
+        for line in email_file:
+            (email, name) = line.split(',')
+            emails[email] = name
+    except FileNotFoundError as err:
+        print(err)
+    return emails
 
-print(emails)
 
-schedule_file = open('schedule.txt', 'r')
+def get_schedule():
+    schedule = ""
+    try:
+        schedule_file = open('schedule.txt', 'r')
+        schedule = schedule_file.read()
+    except FileNotFoundError as err:
+        print(err)
+    return schedule
 
-schedule = schedule_file.read()
 
